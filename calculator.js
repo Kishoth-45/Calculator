3dc53b120c31b03f6fa2e90e1d472e7aa91a7076
KISHOTH KUMAR <79006624+Kishoth-45@users.noreply.github.com>
2024-05-23 12:16:13 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,29 +1,29 @@
 function appendToDisplay(value){
-    var display=document.getElementById("display");
-    if(isValidCharacter(value) ){
-        display.value += value
-    }
+  var display=document.getElementById('display');
+  if(isValidCharacter(value)){
+    display.value += value;
+  }
 }
+
 
 function isValidCharacter(character){
     return /^[0-9+\-*/.%]$/.test(character)
 }
 
 function clearDisplay(){
-    var display=document.getElementById("display");
-    display.value="";
+    var display=document.getElementById('display');
+    display.value="";   
 }
 
 function deleteDisplay(){
-    var display=document.getElementById("display");
-    display.value= display.value.toString().slice(0,-1);
+  var display=document.getElementById('display');
+   display.value= display.value.slice(0,-1)
 }
-
 
 function calculateResult() {
     var display = document.getElementById('display');
     try {
-        display.value = eval(display.value.replace(/[^-()\d/*+.]/g, ''));
+        display.value = eval(display.value);
     } catch (e) {
         display.value = 'Error';
     }
